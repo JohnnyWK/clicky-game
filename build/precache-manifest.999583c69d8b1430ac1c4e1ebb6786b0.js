9cb4514838c66554275883bb1100549085c509e5
Johnny Kreitzman <kreitzka@gmail.com>
2019-11-04 09:29:37 -0600

    deployed app

---
+++ build/precache-manifest.999583c69d8b1430ac1c4e1ebb6786b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36ed79a9a50eac60029fcc217a6c33b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39c890f340addd9f594e",
+    "url": "/static/css/main.1504c6e6.chunk.css"
+  },
+  {
+    "revision": "dcb405b312b8901f0acf",
+    "url": "/static/js/2.d8ad3649.chunk.js"
+  },
+  {
+    "revision": "39c890f340addd9f594e",
+    "url": "/static/js/main.4702e059.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
